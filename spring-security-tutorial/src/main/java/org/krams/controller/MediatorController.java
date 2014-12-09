f5c8e69e5af398b356fab3c112a4a2860e935710
aadebuger <zhvxxh@gmail.com>
2014-12-09 16:05:15 +0800

    Update MediatorController.java

--- spring-security-tutorial/src/main/java/org/krams/controller/MediatorController.java
+++ spring-security-tutorial/src/main/java/org/krams/controller/MediatorController.java
@@ -3,12 +3,24 @@ package org.krams.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Controller
 @RequestMapping("/")
 public class MediatorController {
 
 	@RequestMapping
 	public String getHomePage() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username ;
+		if (principal instanceof UserDetails) {
+  		username = ((UserDetails)principal).getUsername();
+		} else {
+  	username = principal.toString();
+		}
+		System.out.println("username="+username);
+		
 		return "home";
 	}
 	
