4eb9058e3c035802f3fd7173bd8a539fe40113c6
aadebuger <admin@local.host>
2014-12-19 14:31:09 +0800

    auto login

--- spring-security-tutorial/src/main/java/org/krams/controller/AccessController.java
+++ spring-security-tutorial/src/main/java/org/krams/controller/AccessController.java
@@ -2,19 +2,31 @@ package org.krams.controller;
 
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
+import org.krams.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.core.context.SecurityContext;
+
+
+import org.springframework.security.core.userdetails.User;
+
 @Controller
 @RequestMapping
 public class AccessController {
@@ -59,6 +71,51 @@ public class AccessController {
 	@RequestMapping(value = "/create", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
 	public @ResponseBody() String newDepartment(@RequestBody String body,HttpServletResponse response) {
 	
-			return "{\"message\":\"error\"}";
+			return "{\"message\":\"create error\"}";
 		}	
+
+	
+	@Autowired
+	private HttpServletRequest context;
+	
+	@Autowired
+	private CustomUserDetailsService myuserdetailsservice;
+	
+	@RequestMapping(value = "/autologin", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	public @ResponseBody() String autoLogin(@RequestBody String body,HttpServletResponse response) {
+	
+		
+		UserDetails user = myuserdetailsservice.loadUserByUsername("user");
+			
+		authenticateUserAndSetSession(user,context);
+		
+			return "{\"message\":\"create error\"}";
+		}	
+
+	@Autowired
+	@Qualifier("authenticationManager")
+	private AuthenticationManager authenticationManager;
+	
+	private void authenticateUserAndSetSession(UserDetails user, HttpServletRequest request)
+	{
+	     UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+	     user.getUsername(), user.getPassword());
+
+	     // generate session if one doesn't exist
+	     request.getSession();
+
+	     token.setDetails(new WebAuthenticationDetails(request));
+	     Authentication authenticatedUser = authenticationManager.authenticate(token);
+
+	     SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
+	     SecurityContext context = SecurityContextHolder.getContext();
+	
+	     SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
+
+	     request.getSession().setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+
+	     
+	     
+	}
+	
 }
