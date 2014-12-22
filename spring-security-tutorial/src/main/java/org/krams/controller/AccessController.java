0db4cf7f7d629275032e74c0a626d2721b17127a
aadebuger <admin@local.host>
2014-12-22 12:33:03 +0800

    new

--- spring-security-tutorial/src/main/java/org/krams/controller/AccessController.java
+++ spring-security-tutorial/src/main/java/org/krams/controller/AccessController.java
@@ -83,12 +83,11 @@ public class AccessController {
 			return "{\"message\":\"create error\"}";
 		}	
 
-	
+	/*
 	@Autowired
 	private HttpServletRequest context;
 	
-//	@Autowired
-//	private CustomUserDetailsService userDetailsService;
+prvate CustomUserDetailsService userDetailsService;
 	
 	@RequestMapping(value = "/autologin", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
 	public @ResponseBody() String autoLogin(@RequestBody String body,HttpServletResponse response) {
@@ -107,11 +106,7 @@ public class AccessController {
 	@Autowired
 	private UserRepository userRepository;
 
-	/**
-	 * Returns a populated {@link UserDetails} object. 
-	 * The username is first retrieved from the database and then mapped to 
-	 * a {@link UserDetails} object.
-	 */
+	
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		try {
 			org.krams.domain.User domainUser = userRepository.findByUsername(username);
@@ -136,21 +131,12 @@ public class AccessController {
 	}
 	
 	
-	/**
-	 * Retrieves a collection of {@link GrantedAuthority} based on a numerical role
-	 * @param role the numerical role
-	 * @return a collection of {@link GrantedAuthority
-	 */
 	public Collection<? extends GrantedAuthority> getAuthorities(Integer role) {
 		List<GrantedAuthority> authList = getGrantedAuthorities(getRoles(role));
 		return authList;
 	}
 	
-	/**
-	 * Converts a numerical role to an equivalent list of roles
-	 * @param role the numerical role
-	 * @return list of roles as as a list of {@link String}
-	 */
+	
 	public List<String> getRoles(Integer role) {
 		List<String> roles = new ArrayList<String>();
 		
@@ -165,11 +151,7 @@ public class AccessController {
 		return roles;
 	}
 	
-	/**
-	 * Wraps {@link String} roles to {@link SimpleGrantedAuthority} objects
-	 * @param roles {@link String} of roles
-	 * @return list of granted authorities
-	 */
+	
 	public static List<GrantedAuthority> getGrantedAuthorities(List<String> roles) {
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		for (String role : roles) {
@@ -202,5 +184,5 @@ public class AccessController {
 	     
 	     
 	}
-	
+	*/
 }
